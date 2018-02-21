e20099b99cd4cd68a1e35529d4762b93f5dcd154
wangsj96 <wangsijin96@gmail.com>
2018-02-21 20:51:30 +0800

    all updated

---
+++ flix_demo/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  flix_demo
+//
+//  Created by Sijin Wang on 2/14/18.
+//  Copyright © 2018 Sijin Wang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ flix_demo/NowPlayingViewController.swift
@@ -0,0 +1,71 @@
+//
+//  NowPlayingViewController.swift
+//  flix_demo
+//
+//  Created by Sijin Wang on 2/14/18.
+//  Copyright © 2018 Sijin Wang. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies: [[String: Any]] = [];
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self;
+        tableView.estimatedRowHeight = 100
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!;
+        let request = URLRequest(url: url, cachePolicy: URLRequest.CachePolicy.reloadIgnoringLocalCacheData, timeoutInterval: 10);
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main);
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription);
+            }
+            else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any];
+//                print(dataDictionary);
+                let movies = dataDictionary["results"] as! [[String: Any]];
+                self.movies = movies;
+                self.tableView.reloadData();
+            }
+        }
+        task.resume();
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count;
+//        return 10;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath);
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell;
+        let movie = movies[indexPath.row];
+        let title = movie["title"] as! String;
+        let overview = movie["overview"] as! String;
+//        print(overview);
+        cell.titleLabel.text = title;
+        cell.overviewLabel.text = overview;
+        //        print(cell.overviewLabel.text!);
+        
+        let posterPathString = movie["poster_path"] as! String;
+        let posterBaseUrl = "https://image.tmdb.org/t/p/w500";
+        let posterUrl = URL(string: posterBaseUrl + posterPathString)!;
+        cell.posterImageView.af_setImage(withURL: posterUrl);
+        
+        return cell;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
