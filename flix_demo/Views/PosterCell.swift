7827382ebcf5e0557da7ea773197e957a2fb0016
wangsj96 <wangsijin96@gmail.com>
2018-02-25 21:46:13 +0800

    collection view added

---
+++ flix_demo/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_demo
+//
+//  Created by Sijin Wang on 2/25/18.
+//  Copyright © 2018 Sijin Wang. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
